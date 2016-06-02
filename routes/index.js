767d2dee23b268a93dc88a6ca39b94fa371c42a1
liyi <liyi@baijiahulian.com>
2016-06-02 21:08:53 +0800

    博客主页

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title : 'Express'});
 });
+
+router.get('/u/:user', function(req,res) {
+});
+
+router.post('/post', function(req,res) {
+});
+
+router.get('/reg', function(req,res) {
+});
+
+router.post('/reg', function(req,res) {
+});
+
+router.get('/login', function(req,res){
+});
+
+router.post('/login', function(req,res){
+});
+
+router.get('/logout',function(req,res){
+});
+
+
 
 module.exports = router;
